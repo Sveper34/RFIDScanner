e4747f71cfb6e3219d0ae4f8acc31bcaf4948880
Sveper34 <uomaloy@mail.ru>
2023-02-20 22:54:40 +0300

    Правки главной формы

--- app/src/main/java/com/arktika/rfidscanner/ui/Search/SearchFragment.java
+++ app/src/main/java/com/arktika/rfidscanner/ui/Search/SearchFragment.java
@@ -24,8 +24,8 @@ public class SearchFragment extends Fragment {
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        searchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.textHome;
+        //searchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
