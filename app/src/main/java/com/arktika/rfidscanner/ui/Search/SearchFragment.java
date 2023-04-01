2943274b4293d56f2be78bffe54c85c80e5f5556
Sveper34 <uomaloy@mail.ru>
2023-04-01 09:40:23 +0300

    убраны лишниее импорты

--- app/src/main/java/com/arktika/rfidscanner/ui/Search/SearchFragment.java
+++ app/src/main/java/com/arktika/rfidscanner/ui/Search/SearchFragment.java
@@ -3,8 +3,7 @@ package com.arktika.rfidscanner.ui.Search;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.AsyncTask;
+import android.content.IntentFilter;;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -14,23 +13,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import com.arktika.rfidscanner.MainActivity;
 import com.arktika.rfidscanner.R;
 import com.arktika.rfidscanner.databinding.FragmentSearchBinding;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
